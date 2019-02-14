9b3ebbe9f84409eace0eb111574558de1c81d294
tm.0418.tm <tm.0418.tm@gmail.com>
2019-02-14 23:01:31 +0900

    プロジェクト ファイルを追加します。

---
+++ Gorilla/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gorilla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
